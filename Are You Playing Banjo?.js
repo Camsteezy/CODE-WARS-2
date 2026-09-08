17f93373df2270e16e7ab2004152d8bc5f8c4b81
Cameron Crawley <camsteezy@camerons-mbp.mynetworksettings.com>
2026-09-08 12:05:02 -0400

    if your name starts with the letter R or lower case, you are playing the banjo

---
+++ Are You Playing Banjo?.js
@@ -0,0 +1,10 @@
+//Are You Playing Banjo?
+
+function areYouPlayingBanjo(name) {
+  // Implement me
+  if (name[0].toUpperCase() === 'R'){
+  return  name + ' plays banjo'
+  }else {
+    return name + ' does not play banjo'
+  };
+}
